14e1e92f8353008419da9bc132904eb38fd6fdd5
jpdaprato <jpdaprato@gmail.com>
2018-09-30 16:35:00 -0500

    Store environmental variables in the .env file

--- server/index.js
+++ server/index.js
@@ -1,60 +1,69 @@
-'use strict';
+const util = require("util");
 
-var util = require('util');
+const express = require("express");
+const bodyParser = require("body-parser");
+const moment = require("moment");
+const plaid = require("plaid");
+const dotenv = require("dotenv");
 
-var envvar = require('envvar');
-var express = require('express');
-var bodyParser = require('body-parser');
-var moment = require('moment');
-var plaid = require('plaid');
+const result = dotenv.config({
+  path:
+    "/Users/jpdaprato/Dropbox/Hack_Reactor/Immersive/pocket-goblin-client/.env"
+});
 
-var APP_PORT = envvar.number('APP_PORT', 8000);
-var PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID');
-var PLAID_SECRET = envvar.string('PLAID_SECRET');
-var PLAID_PUBLIC_KEY = envvar.string('PLAID_PUBLIC_KEY');
-var PLAID_ENV = envvar.string('PLAID_ENV', 'sandbox');
+if (result.error) {
+  throw result.error;
+}
+
+const APP_PORT = process.env.APP_PORT;
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
+const PLAID_SECRET = process.env.PLAID_SECRET;
+const PLAID_PUBLIC_KEY = process.env.PLAID_PUBLIC_KEY;
+const PLAID_ENV = process.env.PLAID_ENV;
 
 // We store the access_token in memory - in production, store it in a secure
 // persistent data store
-var ACCESS_TOKEN = 'access-sandbox-ea589d77-c204-4378-8767-5685aad07a88';
-var PUBLIC_TOKEN = null;
-var ITEM_ID = 'JvEGJGRWGotMQwxEGBrpi3JWn3l4raCdDp8w4';
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const PUBLIC_TOKEN = null;
+const ITEM_ID = process.env.ITEM_ID;
 
 // Initialize the Plaid client
 // Find your API keys in the Dashboard (https://dashboard.plaid.com/account/keys)
-var client = new plaid.Client(
+const client = new plaid.Client(
   PLAID_CLIENT_ID,
   PLAID_SECRET,
   PLAID_PUBLIC_KEY,
   plaid.environments[PLAID_ENV],
-  {version: '2018-05-22'}
+  { version: "2018-05-22" }
 );
 
-var app = express();
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
 
-app.get('/', function(request, response, next) {
-  response.render('index.ejs', {
+app.get("/", function(request, response, next) {
+  response.render("index.ejs", {
     PLAID_PUBLIC_KEY: PLAID_PUBLIC_KEY,
-    PLAID_ENV: PLAID_ENV,
+    PLAID_ENV: PLAID_ENV
   });
 });
 
 // Exchange token flow - exchange a Link public_token for
 // an API access_token
 // https://plaid.com/docs/#exchange-token-flow
-app.post('/get_access_token', function(request, response, next) {
+app.post("/get_access_token", function(request, response, next) {
   PUBLIC_TOKEN = request.body.public_token;
   client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
     if (error != null) {
       prettyPrintResponse(error);
       return response.json({
-        error: error,
+        error: error
       });
     }
     ACCESS_TOKEN = tokenResponse.access_token;
@@ -63,97 +72,104 @@ app.post('/get_access_token', function(request, response, next) {
     response.json({
       access_token: ACCESS_TOKEN,
       item_id: ITEM_ID,
-      error: null,
+      error: null
     });
   });
 });
 
-
 // Retrieve Transactions for an Item
 // https://plaid.com/docs/#transactions
-app.get('/transactions', function(request, response, next) {
+app.get("/transactions", function(request, response, next) {
   // Pull transactions for the Item for the last 30 days
-  var startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
-  var endDate = moment().format('YYYY-MM-DD');
-  client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
-    count: 250,
-    offset: 0,
-  }, function(error, transactionsResponse) {
+  let startDate = moment()
+    .subtract(30, "days")
+    .format("YYYY-MM-DD");
+  let endDate = moment().format("YYYY-MM-DD");
+  client.getTransactions(
+    ACCESS_TOKEN,
+    startDate,
+    endDate,
+    {
+      count: 250,
+      offset: 0
+    },
+    function(error, transactionsResponse) {
+      if (error != null) {
+        prettyPrintResponse(error);
+        return response.json({
+          error: error
+        });
+      } else {
+        prettyPrintResponse(transactionsResponse);
+        response.json({ error: null, transactions: transactionsResponse });
+      }
+    }
+  );
+});
+
+// Retrieve Identity for an Item
+// https://plaid.com/docs/#identity
+app.get("/identity", function(request, response, next) {
+  client.getIdentity(ACCESS_TOKEN, function(error, identityResponse) {
     if (error != null) {
       prettyPrintResponse(error);
       return response.json({
         error: error
       });
-    } else {
-      prettyPrintResponse(transactionsResponse);
-      response.json({error: null, transactions: transactionsResponse});
-    }
-  });
-});
-
-// Retrieve Identity for an Item
-// https://plaid.com/docs/#identity
-app.get('/identity', function(request, response, next) {
-  client.getIdentity(ACCESS_TOKEN, function(error, identityResponse) {
-    if (error != null) {
-      prettyPrintResponse(error);
-      return response.json({
-        error: error,
-      });
     }
     prettyPrintResponse(identityResponse);
-    response.json({error: null, identity: identityResponse});
+    response.json({ error: null, identity: identityResponse });
   });
 });
 
 // Retrieve real-time Balances for each of an Item's accounts
 // https://plaid.com/docs/#balance
-app.get('/balance', function(request, response, next) {
+app.get("/balance", function(request, response, next) {
   client.getBalance(ACCESS_TOKEN, function(error, balanceResponse) {
     if (error != null) {
       prettyPrintResponse(error);
       return response.json({
-        error: error,
+        error: error
       });
     }
     prettyPrintResponse(balanceResponse);
-    response.json({error: null, balance: balanceResponse});
+    response.json({ error: null, balance: balanceResponse });
   });
 });
 
 // Retrieve an Item's accounts
 // https://plaid.com/docs/#accounts
-app.get('/accounts', function(request, response, next) {
+app.get("/accounts", function(request, response, next) {
   client.getAccounts(ACCESS_TOKEN, function(error, accountsResponse) {
     if (error != null) {
       prettyPrintResponse(error);
       return response.json({
-        error: error,
+        error: error
       });
     }
     prettyPrintResponse(accountsResponse);
-    response.json({error: null, accounts: accountsResponse});
+    response.json({ error: null, accounts: accountsResponse });
   });
 });
 
 // Retrieve ACH or ETF Auth data for an Item's accounts
 // https://plaid.com/docs/#auth
-app.get('/auth', function(request, response, next) {
+app.get("/auth", function(request, response, next) {
   client.getAuth(ACCESS_TOKEN, function(error, authResponse) {
     if (error != null) {
       prettyPrintResponse(error);
       return response.json({
-        error: error,
+        error: error
       });
     }
     prettyPrintResponse(authResponse);
-    response.json({error: null, auth: authResponse});
+    response.json({ error: null, auth: authResponse });
   });
 });
 
 // Retrieve information about an Item
 // https://plaid.com/docs/#retrieve-item
-app.get('/item', function(request, response, next) {
+app.get("/item", function(request, response, next) {
   // Pull the Item - this includes information about available products,
   // billed products, webhook information, and more.
   client.getItem(ACCESS_TOKEN, function(error, itemResponse) {
@@ -164,10 +180,14 @@ app.get('/item', function(request, response, next) {
       });
     }
     // Also pull information about the institution
-    client.getInstitutionById(itemResponse.item.institution_id, function(err, instRes) {
+    client.getInstitutionById(itemResponse.item.institution_id, function(
+      err,
+      instRes
+    ) {
       if (err != null) {
-        var msg = 'Unable to pull institution information from the Plaid API.';
-        console.log(msg + '\n' + JSON.stringify(error));
+        const msg =
+          "Unable to pull institution information from the Plaid API.";
+        console.log(msg + "\n" + JSON.stringify(error));
         return response.json({
           error: msg
         });
@@ -175,27 +195,27 @@ app.get('/item', function(request, response, next) {
         prettyPrintResponse(itemResponse);
         response.json({
           item: itemResponse.item,
-          institution: instRes.institution,
+          institution: instRes.institution
         });
       }
     });
   });
 });
 
-var server = app.listen(APP_PORT, function() {
-  console.log('plaid-quickstart server listening on port ' + APP_PORT);
+const server = app.listen(APP_PORT, function() {
+  console.log("PocketGoblin server listening on port " + APP_PORT);
 });
 
-var prettyPrintResponse = response => {
-  console.log(util.inspect(response, {colors: true, depth: 4}));
+const prettyPrintResponse = response => {
+  console.log(util.inspect(response, { colors: true, depth: 4 }));
 };
 
-app.post('/set_access_token', function(request, response, next) {
+app.post("/set_access_token", function(request, response, next) {
   ACCESS_TOKEN = request.body.access_token;
   client.getItem(ACCESS_TOKEN, function(error, itemResponse) {
     response.json({
       item_id: itemResponse.item.item_id,
-      error: false,
+      error: false
     });
   });
 });
