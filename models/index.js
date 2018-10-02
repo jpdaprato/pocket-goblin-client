c69a0b6ca4b8662de7d0d1abaa5ed511fd718ce6
Charlie Astrada <carlosastrada@gmail.com>
2018-10-01 22:07:10 -0300

    Add variables to connection to make things easier when we move to env variables.
    * Add dbName, dbUser, dbPassword, and dbHost

--- models/index.js
+++ models/index.js
@@ -1,7 +1,14 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("pocketgoblin", "charlieastrada", "", {
-  dialect: "postgres"
+//FIXME:Move these things into environmental variables
+const dbName = process.env.DATABASE_NAME || "pocketgoblin";
+const dbUser = process.env.DATABASE_USER || "charlieastrada";
+const dbPassword = process.env.DATABASE_PASSWORD || "";
+const dbHost = process.env.HOST || "localhost";
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  dialect: "postgres",
+  host: dbHost
 });
 
 const models = {
