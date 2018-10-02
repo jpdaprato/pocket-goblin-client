b201e9d6b67e285484acc0a0299f9b93717e4889
anthonySchafrik <anthonySchafrik@gmail.com>
2018-10-02 19:06:10 -0400

    trying to fix git probs

--- src/components/FinancialSnapShot.jsx
+++ src/components/FinancialSnapShot.jsx
@@ -1,7 +1,19 @@
 import React form 'react';
+import CashFlow from './CashFlow';
 
-const FinancialSnapShot = () => {
-  return (
-    <h1>HI</h1>
-  )
+class FinancialSnapShot extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+       debtToSaving: 0,
+       saving: 0,
+    }
+  }
+  render() {
+    return (
+      <CashFlow />
+    )
+  }
 }
+
+export default FinancialSnapShot;
