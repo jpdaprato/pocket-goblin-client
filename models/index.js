b0e4518866433d0912cbb8576dab7f4e5e5db3ff
Charlie Astrada <carlosastrada@gmail.com>
2018-10-01 21:46:30 -0300

    Add centralized index.js for models to simplify export management of future models .
    * loops through all models and creates associations.
    * exports models objects

--- models/index.js
+++ models/index.js
@@ -1,14 +1,20 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("pocketgoblin", "postgres", "postgres");
+const sequelize = new Sequelize("pocketgoblin", "charlieastrada", "", {
+  dialect: "postgres"
+});
 
 const models = {
-  user: sequelize.import("./users")
+  Account: sequelize.import("./account"),
+  Client: sequelize.import("./client"),
+  Environment: sequelize.import("./environment"),
+  Item: sequelize.import("./item"),
+  Transaction: sequelize.import("./transaction")
 };
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+Object.keys(models).forEach(model => {
+  if ("associate" in models[model]) {
+    models[model].associate(models);
   }
 });
 
