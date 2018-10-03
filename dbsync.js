4c1100c91e881b92e229aeaf0a9c9f825a96a336
Charlie Astrada <carlosastrada@gmail.com>
2018-10-03 02:20:35 -0300

    Refactor dbsync to only sync models.
    * comment out the env and client model syncs
    * add comments to clarify

--- dbsync.js
+++ dbsync.js
@@ -1,5 +1,28 @@
 const models = require("./models/index");
+const dotenv = require("dotenv");
+const result = dotenv.config();
 
-models.sequelize.sync({ force: true }).then(() => {
-  console.log("sync complete");
-});
+if (result.error) {
+  throw result.error;
+}
+
+//Create environment and client entries to avoid problems when
+// pulling in transactions, accounts and items
+models.sequelize
+  // //use force:true to destroy data/tables before sync (ok for dev)
+  // .sync({ force: true })
+  .sync()
+  // .then(() => {
+  //   return models.Environment.create({
+  //     type: process.env.PLAID_ENV,
+  //     secret: process.env.PLAID_SECRET
+  //   });
+  // })
+  // .then(() => {
+  //   return models.Client.create({
+  //     plaid_client_id: process.env.PLAID_CLIENT_ID,
+  //     environment_id: 1
+  //   });
+  // })
+  //.then(() => models.sequelize.close())
+  .catch(err => console.error(err));
