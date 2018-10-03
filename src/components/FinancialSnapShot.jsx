d882d5470d804c73b3d4120b1b8af96d824e473f
anthonySchafrik <anthonySchafrik@gmail.com>
2018-10-02 22:20:16 -0400

    add debt components

--- src/components/FinancialSnapShot.jsx
+++ src/components/FinancialSnapShot.jsx
@@ -1,18 +1,33 @@
-import React form 'react';
-import CashFlow from './CashFlow';
+import React from "react";
+import CashFlow from "./CashFlow";
+import DebtToSavingRatio from "./DebtToSavingRatio";
+import Header from "./Header";
+
+const divStyle = {
+  // border: "5px solid yellow",
+  padding: "5px",
+  marginTop: "5px",
+  marginBottom: "15px"
+};
 
 class FinancialSnapShot extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-       debtToSaving: 0,
-       saving: 0,
-    }
+      debtToSaving: 0,
+      saving: 0
+    };
   }
   render() {
     return (
-      <CashFlow />
-    )
+      <div style={divStyle}>
+        <Header />
+        <CashFlow />
+        <div>
+          <DebtToSavingRatio />
+        </div>
+      </div>
+    );
   }
 }
 
