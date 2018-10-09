e0a0fc08a91b03f1a15e0158ac2602dc08ebbb28
Charlie Astrada <carlosastrada@gmail.com>
2018-10-09 00:02:06 -0300

    Add a component to render a Pay Debt or Invest It component depending on state
    * Conditions include: pay debt vs invest; amount recurring vs non-recurring

---
+++ src/components/PayDebtOrInvestIt.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import PayDebtRecurring from "./PayDebtRecurring.jsx";
+import PayDebtNonRecurring from "./PayDebtNonRecurring.jsx";
+import InvestItRecurring from "./InvestItRecurring.jsx";
+import InvestItNonRecurring from "./InvestItNonRecurring.jsx";
+
+const PayDebtOrInvestIt = props => {
+  const isRecurring = !!(props.purchaseFrequency !== "never");
+  const isPayDebtSelected = !!(props.payDebtOrInvestItSelection === "debt");
+
+  //"Pay Debt" selected && Purchase Amount Repeats Never
+  if (isPayDebtSelected && !isRecurring) {
+    return (
+      <PayDebtNonRecurring
+        debtFreeFasterBy={props.debtFreeFasterBy}
+        interestSavedAmount={props.interestSavedAmount}
+        totalSavedAmount={props.totalSavedAmount}
+        purchaseAmount={props.purchaseAmount}
+      />
+    );
+  }
+
+  //"Pay Debt" selected && Purchase Amount Repeats
+  if (isPayDebtSelected && isRecurring) {
+    return (
+      <PayDebtRecurring
+        debtFreeFasterBy={props.debtFreeFasterBy}
+        interestSavedAmount={props.interestSavedAmount}
+        totalSavedAmount={props.totalSavedAmount}
+        purchaseAmount={props.purchaseAmount}
+      />
+    );
+  }
+
+  //"Invest It" selected && Purchase Amount Repeats Never
+  if (isPayDebtSelected === false && isRecurring === false) {
+    return (
+      <InvestItNonRecurring
+        investmentTimeline={props.investmentTimeline}
+        purchaseAmount={props.purchaseAmount}
+        rateOfReturn={props.rateOfReturn}
+      />
+    );
+  }
+
+  //"Invest It" selected && Purchase Amount Repeats
+  if (isPayDebtSelected === false && isRecurring) {
+    return (
+      <InvestItRecurring
+        investmentTimeline={props.investmentTimeline}
+        purchaseAmount={props.purchaseAmount}
+        rateOfReturn={props.rateOfReturn}
+      />
+    );
+  }
+};
+
+export default PayDebtOrInvestIt;
